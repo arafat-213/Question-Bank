67af7472271521e39ffdd9eba7062bd00f31db08
arafat-213 <tai.arafat.at@gmail.com>
2017-09-05 20:56:22 +0530

    added question no21.problem1

---
+++ Question Bank/src/Question_21/Problem1/Vegetable.java
@@ -0,0 +1,47 @@
+package Question_21.Problem1;
+
+public abstract class Vegetable {
+	String color;
+	String name;
+	//public abstract String  toString();
+	public abstract String sayMyName();
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return sayMyName();
+	}
+	
+}
+	
+class Potato extends Vegetable {
+		
+	Potato() {
+		color="brown";
+	}
+	
+	public String sayMyName() {
+		return"They call me Potato"+" Because my color is : "+color;
+	}
+}
+	
+class Brinjal extends Vegetable {
+	Brinjal() {
+		color="violet";
+	}
+	
+	public String sayMyName() {
+		//System.out.println("No one likes me.. :-(");
+		return"I am a Brinjal and No one likes me.. :-("+" Because my color is : "+color;
+	}
+}
+	
+class Tomato extends Vegetable{
+	Tomato() {
+		color="red";
+	}
+	
+	public String sayMyName() {
+		//System.out.println("I am a vegetable yet some idiots add me in fruit salad :O ");
+		return"I am a vegetable yet some idiots add me in fruit salad :O "+" Because my color is : "+color;
+	}
+}
